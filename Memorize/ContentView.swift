afbce7a02b3f38f29406acd13e34f95009725658
A menina do gorro {Ana Raiany} <101668266+Ameninadogorro@users.noreply.github.com>
2023-01-13 23:18:00 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Ana Raiany Guimarães Gomes on 2023-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Hello, world!")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ana Raiany Guimarães Gomes on 2023-01-11.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
